35307a420aebef5a74d623e8e64cb977f5e97f1b
Ram Pasala <ram.pasala7@gmail.com>
2018-12-12 00:02:04 +0530

    chore(coverage):added istanbul puppeteer coverage

--- src/pages/pageHelper.ts
+++ src/pages/pageHelper.ts
@@ -19,7 +19,11 @@ class PageHelper {
         dumpio: false,
       });
       this.page = await this.browser.newPage();
-    } catch (Exception) {
+      await Promise.all([
+        this.page.coverage.startJSCoverage(),
+        this.page.coverage.startCSSCoverage(),
+      ]);
+    } catch (Exception) /* istanbul ignore next */ {
       throw new Error(Exception.toString());
     }
   }
@@ -32,7 +36,7 @@ class PageHelper {
     while (i < this.retryCount) {
       try {
         return await this.page.goto(url);
-      } catch (Exception) {
+      } catch (Exception) /* istanbul ignore next */ {
         i++;
         if (i === this.retryCount) {
           throw new Error(Exception.toString());
@@ -48,7 +52,7 @@ class PageHelper {
     while (i < this.retryCount) {
       try {
         return await this.page.title();
-      } catch (Exception) {
+      } catch (Exception) /* istanbul ignore next */ {
         i++;
         if (i === this.retryCount) {
           throw new Error(Exception.toString());
@@ -56,99 +60,7 @@ class PageHelper {
       }
     }
   }
-  /**
-   * @param  {any} fn
-   * @returns Promise
-   */
-  public async evaluate(fn: any): Promise<any> {
-    let i: number = 0;
-    while (i < this.retryCount) {
-      try {
-        return await this.page.evaluate(fn);
-      } catch (Exception) {
-        i++;
-        if (i === this.retryCount) {
-          throw new Error(Exception.toString());
-        }
-      }
-    }
-  }
-  /**
-   * @param  {string} element
-   * @returns Promise
-   */
-  public async waitForElement(element: string): Promise<ElementHandle> {
-    let i: number = 0;
-    while (i < this.retryCount) {
-      try {
-        return await this.page.waitForSelector(element);
-      } catch (Exception) {
-        i++;
-        if (i === this.retryCount) {
-          throw new Error(Exception.toString());
-        }
-      }
-    }
-  }
-  /**
-   * @param  {string} element
-   * @returns Promise
-   */
-  public async $(element: string): Promise<ElementHandle> {
-    let i: number = 0;
-    while (i < this.retryCount) {
-      try {
-        return await this.page.$(element);
-      } catch (Exception) {
-        i++;
-        if (i === this.retryCount) {
-          throw new Error(Exception.toString());
-        }
-      }
-    }
-  }
-  /**
-   * @param  {string} element
-   * @returns Promise
-   */
-  public async $$(element: string): Promise<ElementHandle[]> {
-    let i: number = 0;
-    while (i < this.retryCount) {
-      try {
-        return await this.page.$$(element);
-      } catch (Exception) {
-        i++;
-        if (i === this.retryCount) {
-          throw new Error(Exception.toString());
-        }
-      }
-    }
-  }
-  /**
-   * @param  {string} element
-   * @returns Promise
-   */
-  public async focusElement(element: string): Promise<void> {
-    let i: number = 0;
-    while (i < this.retryCount) {
-      try {
-        await this.page.waitForSelector(element);
-        return await this.page.focus(element);
-      } catch (Exception) {
-        try {
-          i++;
-          await this.page.waitForSelector(element);
-          await this.page.evaluate('arguments[0].click()', element);
-          continue;
-        } catch (Exception) {
-          i++;
-          if (i === this.retryCount) {
-            throw new Error(Exception.toString());
-          }
-        }
-      }
-    }
-  }
+
   /**
    * @param  {string} element
    * @returns Promise
@@ -159,13 +71,13 @@ class PageHelper {
       try {
         await this.page.waitForSelector(element);
         return await this.page.click(element);
-      } catch (Exception) {
+      } catch (Exception) /* istanbul ignore next */ {
         try {
           i++;
           await this.page.waitForSelector(element);
           await this.page.evaluate('arguments[0].click()', element);
           continue;
-        } catch (Exception) {
+        } catch (Exception) /* istanbul ignore next */ {
           i++;
           if (i === this.retryCount) {
             throw new Error(Exception.toString());
@@ -176,22 +88,21 @@ class PageHelper {
   }
   /**
    * @param  {string} element
+   * @param  {string} text
    * @returns Promise
    */
-  public async xpathClick(element: string): Promise<void> {
+  public async sendElementText(element: string, text: string): Promise<void> {
     let i: number = 0;
     while (i < this.retryCount) {
       try {
-        await this.page.waitForXPath(element);
-        const xpathElement = await this.page.$x(element);
-        return await xpathElement[0].click();
-      } catch (Exception) {
+        return await this.page.type(element, text);
+      } catch (Exception) /* istanbul ignore next */ {
         try {
           i++;
-          await this.page.waitForXPath(element);
+          await this.page.waitForSelector(element);
           await this.page.evaluate('arguments[0].click()', element);
           continue;
-        } catch (Exception) {
+        } catch (Exception) /* istanbul ignore next */ {
           i++;
           if (i === this.retryCount) {
             throw new Error(Exception.toString());
@@ -211,38 +122,13 @@ class PageHelper {
         const elementHandle = await this.page.$(element);
         await elementHandle.click({ clickCount: 3 });
         return await elementHandle.press('Backspace');
-      } catch (Exception) {
+      } catch (Exception) /* istanbul ignore next */ {
         try {
           i++;
           await this.page.waitForSelector(element);
           await this.page.evaluate('arguments[0].click()', element);
           continue;
-        } catch (Exception) {
-          i++;
-          if (i === this.retryCount) {
-            throw new Error(Exception.toString());
-          }
-        }
-      }
-    }
-  }
-  /**
-   * @param  {string} element
-   * @param  {string} text
-   * @returns Promise
-   */
-  public async sendElementText(element: string, text: string): Promise<void> {
-    let i: number = 0;
-    while (i < this.retryCount) {
-      try {
-        return await this.page.type(element, text);
-      } catch (Exception) {
-        try {
-          i++;
-          await this.page.waitForSelector(element);
-          await this.page.evaluate('arguments[0].click()', element);
-          continue;
-        } catch (Exception) {
+        } catch (Exception) /* istanbul ignore next */ {
           i++;
           if (i === this.retryCount) {
             throw new Error(Exception.toString());
@@ -260,7 +146,7 @@ class PageHelper {
     while (i < this.retryCount) {
       try {
         return await this.page.keyboard.press(keys);
-      } catch (Exception) {
+      } catch (Exception) /* istanbul ignore next */ {
         i++;
         if (i === this.retryCount) {
           throw new Error(Exception.toString());
@@ -273,117 +159,25 @@ class PageHelper {
    * @param  {any} answers
    * @returns Promise
    */
-  public async enterText(element: ElementHandle, text: string): Promise<void> {
-    try {
-      await element.focus();
-      await element.type('');
-      await element.type(text);
-      await this.enterKeys('Enter');
-      await this.page.waitFor(500);
-    } catch (Exception) {
-      throw new Error(Exception.toString());
-    }
-  }
-  /**
-   * @param  {string} element
-   * @returns Promise
-   */
-  public async getText(element: string): Promise<string[]> {
-    let i: number = 0;
-    while (i < this.retryCount) {
-      try {
-        return await this.page.evaluate((element) => {
-          const els = Array.from(document.querySelectorAll(element));
-          return els.map((el) => {
-            return el.textContent;
-          });
-        }, element);
-      } catch (Exception) {
-        try {
-          i++;
-          await this.page.waitForSelector(element);
-          await this.page.evaluate('arguments[0].click()', element);
-          continue;
-        } catch (Exception) {
-          i++;
-          if (i === this.retryCount) {
-            throw new Error(Exception.toString());
-          }
-        }
-      }
-    }
-  }
-  /**
-   * @param  {any} options?
-   * @returns Promise
-   */
-  public async waitForNavigation(options?: any): Promise<Response> {
-    try {
-      return await this.page.waitForNavigation(options);
-    } catch (Exception) {
-      throw new Error(Exception.toString());
-    }
-  }
-  /**
-   * @param  {string} element
-   * @returns Promise
-   */
-  public async isElementExists(element: string): Promise<boolean> {
-    let i: number = 0;
-    while (i < this.retryCount) {
-      try {
-        await this.page.waitForSelector(element, { timeout: 1000 });
-        if ((await this.page.$(element)) !== null) {
-          return true;
-        } else {
-          return false;
-        }
-      } catch (Exception) {
-        i++;
-        if (i === this.retryCount) {
-          return false;
-        }
-      }
-    }
-  }
-  /**
-   * @param  {string} element
-   * @returns Promise
-   */
-  public async isElementVisible(element: string): Promise<boolean> {
-    let i: number = 0;
-    while (i < this.retryCount) {
-      try {
-        const isVisible = await this.page.evaluate(() => {
-          const el = document.querySelector(element);
-          if (!el) {
-            return false;
-          }
-          const style = window.getComputedStyle(el);
-          return style && style.display !== 'none' && style.visibility !== 'hidden' &&
-            style.opacity !== '0';
-        });
-        return isVisible;
-      } catch (Exception) {
-        i++;
-        if (i === this.retryCount) {
-          throw new Error(Exception.toString());
-        }
-      }
-    }
-  }
 
   public async screenshot(): Promise<any> {
     try {
       return await this.page.screenshot();
-    } catch (Exception) {
+    } catch (Exception) /* istanbul ignore next */ {
       throw new Error(Exception.toString());
     }
   }
   public async close() {
     try {
+      const pti = require('puppeteer-to-istanbul');
+      const [jsCoverage, cssCoverage] = await Promise.all([
+        this.page.coverage.stopJSCoverage(),
+        this.page.coverage.stopCSSCoverage(),
+      ]);
+      pti.write(jsCoverage);
+      pti.write(cssCoverage);
       return await this.browser.close();
-    } catch (Exception) {
+    } catch (Exception) /* istanbul ignore next */ {
       throw new Error(Exception.toString());
     }
   }
